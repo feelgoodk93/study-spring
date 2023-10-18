ede6ec87dd0366fc01beb7e41e204a4d940b76c9
feelgoodk93 <feelgoodk93@gmail.com>
2023-10-18 15:25:15 +0900

    스프링 핵심 원리 이해1 - 회원 도메인 실행과 테스트

--- 인프런 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 인프런 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
